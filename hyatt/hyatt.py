445c9ff9412376dae0512f168d533139c155aba2
tz1006 <a@omg.tf>
2019-05-03 17:21:02 +0800

    Update hyatt.py

--- hyatt/hyatt.py
+++ hyatt/hyatt.py
@@ -1,1 +1,123 @@
 
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+# filename: hiltoncode.py
+
+import requests
+from datetime import datetime, timedelta
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+
+
+driver = webdriver.Chrome('./chromedriver')
+driver.set_window_size(800, 900)
+js = r"Object.defineProperty(navigator, 'webdriver', {get: () => undefined,});"                           
+driver.execute_script(js)
+
+
+
+def datechange(date, change):
+    change = int(change)
+    date = datetime.strptime(date, '%Y-%m-%d').date()
+    new_date = date + timedelta(days=change)
+    new_date_string = new_date.strftime('%Y-%m-%d')
+    return new_date_string
+
+
+
+
+
+def get_cn_hotels():
+    # 生成url
+    url = 'https://www.hyatt.com/zh-CN/explore-hotels/partial?'
+    payload = {'regionGroup': '5-Asia',
+               'categories': '',
+               'brands': ''}
+    url += urlencode(payload)
+    #print(url)
+    # soup
+    with requests.session() as s:
+        r = s.get(url)
+    html = r.content
+    soup = BeautifulSoup(html, "html.parser")
+    cn_soup = soup.find_all('li', attrs={'data-js-country':'大中华地区'})[0]
+    cn_hotels = cn_soup.find_all('li', attrs={'class':'property b-mb2'})
+    return cn_hotels
+
+
+def cn_hotels_list():
+    cn_hotels = get_cn_hotels()
+    hotel_list = []
+    for i in cn_hotels:
+        name = i.a.text
+        code = i.get('data-js-property')
+        link = i.a.get('href')
+        span = i.a.span
+        if span == None:
+            tag = ''
+        else:
+            tag = span.text
+        d = {'name': name,
+             'code': code,
+             'link': link,
+             'tag': tag}
+        hotel_list.append(d)
+    return hotel_list
+
+
+def quote_price(hotel_code, date, promo=False):
+    change = 1
+    offercode = ''
+    if promo:
+        offercode = 'CUP19'
+        change = 3
+    checkoutdate = datechange(date, change)
+    # url
+    url = 'https://www.hyatt.com/zh-CN/shop/rates/%s?' % hotel_code
+    payload = {'rooms': 1,
+               'adults': 1,
+               'checkinDate': date,
+               'checkoutDate': checkoutdate,
+               'kids': 0,
+               'offercode': offercode,
+               'rateFilter': 'standard'}
+    url += urlencode(payload)
+    #print(url)
+    # soup
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+    lowest_price = soup.find_all('div', attrs={'class':'b-text_weight-bold rate-pricing'})[0]
+    return lowest_price
+               
+               
+               
+    rooms=1&adults=1&checkinDate=2019-05-15&checkoutDate=2019-05-16&kids=0&offercode=CUP19&rateFilter=standard
+
+
+l = cn_hotels_list()
+
+a = quote_price('jjnye','2019-05-16')
+
+
+# test
+headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Mobile Safari/537.36',
+           'referer': 'https://www.hyatt.com/zh-CN/shop/rates/czxrz?rooms=1&adults=1&checkinDate=2019-05-16&checkoutDate=2019-05-17&kids=0&offercode=&rateFilter=standard'
+          }
+
+url = 'https://www.hyatt.com/zh-CN/shop/rates/czxrz?rooms=1&adults=1&checkinDate=2019-05-16&checkoutDate=2019-05-17&kids=0&offercode=&rateFilter=standard'
+
+url = 'https://www.hyatt.com/zh-CN/shop/rates/czxrz?rooms=1&adults=1&checkinDate=2019-05-16&checkoutDate=2019-05-17&kids=0&offercode=&rateFilter=standard'
+
+
+with requests.session() as s:
+    rr = s.get(url, headers=headers)
+    rr = s.get(url, headers=headers)
+
+print(rr.status_code)
+
+
+
+
