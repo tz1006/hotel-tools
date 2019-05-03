f1f8dc00757b9dea566bf6f115af9ab23c897f31
tz1006 <a@omg.tf>
2019-05-03 18:20:01 +0800

    Create webdriver.py

---
+++ hyatt/webdriver.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+# filename: webdriver.py
+
+
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
+
+option = ChromeOptions()
+option.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+
+class driver():
+    def __init__(self, limit=10):
+        self.create_driver()
+        self.limit = limit
+    def create_driver(self):
+        self.driver = Chrome('./chromedriver', options=option)
+        self.driver.set_window_size(800, 900)
+        self.count = 0
+    def delete_driver(self):
+        self.driver.exit()
+    def get_page(self, url):
+        if self.count == self.limit:
+            self.create_driver()
+            print('重启浏览器')
+        self.driver.get(url)
+        html = self.driver.page_source
+        return html
+    
