a5dbdc29bd413076a06d4234ed3d82cb8d98dcca
tz1006 <a@omg.tf>
2019-05-05 17:34:30 +0800

    Update flask.py

--- hyatt/flask.py
+++ hyatt/flask.py
@@ -7,15 +7,27 @@ from flask import Flask,render_template
 
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
-@app.route('/hyatt/<str:date>')
-def index():
-    path = 'data/%s' % date
-    with open(path, 'r') as f:
-        data = json.loads(f)
-    return render_template('template.html', date=date, data=data)
 
+@app.route('/hyatt/<string:date>', methods=['GET'])
+def get_page(date):
+    print(date)
+    path = 'data/%s.json' % date
+    with open(path, 'r', encoding='utf-8') as f:
+        j = f.read()
+        #print(j)
+        #return j
+        hotels = json.loads(j)
+    return render_template('template.html', date=date, hotels=hotels)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=2779, debug=False)
+
+    
+#path = 'data/2019-05-08.json'
+#with open(path, 'r', encoding='utf-8') as f:
+#    j = f.read()
+#
+#data = json.loads(j)
