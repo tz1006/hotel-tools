53585bee6cdabc57681ca4258589b7fdb2d9065b
tz1006 <a@omg.tf>
2019-05-05 18:19:22 +0800

    Update flask.py

--- hyatt/flask.py
+++ hyatt/flask.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*- 
 # filename: flask.py
 
+import os
 import json
 from flask import Flask,render_template
 
@@ -12,14 +13,17 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route('/hyatt/<string:date>', methods=['GET'])
 def get_page(date):
-    print(date)
     path = 'data/%s.json' % date
-    with open(path, 'r', encoding='utf-8') as f:
-        j = f.read()
-        #print(j)
-        #return j
-        hotels = json.loads(j)
-    return render_template('template.html', date=date, hotels=hotels)
+    # 文件存在
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as f:
+            j = f.read()
+            #print(j)
+            #return j
+            hotels = json.loads(j)
+        return render_template('template.html', date=date, hotels=hotels)
+    else:
+        pass
 
 
 if __name__ == '__main__':
